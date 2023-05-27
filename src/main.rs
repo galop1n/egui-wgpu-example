f88acade563bd65b8fe9a44676864212b6a222ed
galop1n <galopin@gmail.com>
2023-05-26 22:23:37 -0700

    switch to egui-wgpu, egui-winit and egui 0.22

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use std::iter;
 use std::time::Instant;
 
-use ::egui::FontDefinitions;
 use chrono::Timelike;
-use egui_wgpu_backend::{RenderPass, ScreenDescriptor};
-use egui_winit_platform::{Platform, PlatformDescriptor};
+use egui_wgpu::renderer::ScreenDescriptor;
+use egui_wgpu::Renderer;
+use egui_winit::State;
+
+use wgpu::InstanceDescriptor;
 use winit::event::Event::*;
 use winit::event_loop::ControlFlow;
 const INITIAL_WIDTH: u32 = 1920;
@@ -32,7 +34,7 @@ fn main() {
         .with_decorations(true)
         .with_resizable(true)
         .with_transparent(false)
-        .with_title("egui-wgpu_winit example")
+        .with_title("egui-wgpu-winit example")
         .with_inner_size(winit::dpi::PhysicalSize {
             width: INITIAL_WIDTH,
             height: INITIAL_HEIGHT,
@@ -40,8 +42,12 @@ fn main() {
         .build(&event_loop)
         .unwrap();
 
-    let instance = wgpu::Instance::new(wgpu::Backends::PRIMARY);
-    let surface = unsafe { instance.create_surface(&window) };
+    let instance_descriptor = InstanceDescriptor {
+        backends: wgpu::Backends::PRIMARY,
+        ..InstanceDescriptor::default()
+    };
+    let instance = wgpu::Instance::new(instance_descriptor);
+    let surface = unsafe { instance.create_surface(&window).unwrap() };
 
     // WGPU 0.11+ support force fallback (if HW implementation not supported), set it to true or false (optional).
     let adapter = pollster::block_on(instance.request_adapter(&wgpu::RequestAdapterOptions {
@@ -61,41 +67,47 @@ fn main() {
     ))
     .unwrap();
 
+    let capabilities = surface.get_capabilities(&adapter);
+    let surface_format = *capabilities.formats.iter().find(|f| f.is_srgb()).unwrap();
+
     let size = window.inner_size();
-    let surface_format = surface.get_supported_formats(&adapter)[0];
     let mut surface_config = wgpu::SurfaceConfiguration {
         usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
         format: surface_format,
-        width: size.width as u32,
-        height: size.height as u32,
+        width: size.width,
+        height: size.height,
         present_mode: wgpu::PresentMode::Fifo,
+        alpha_mode: capabilities.alpha_modes[0],
+        view_formats: vec![],
     };
     surface.configure(&device, &surface_config);
 
-    // We use the egui_winit_platform crate as the platform.
-    let mut platform = Platform::new(PlatformDescriptor {
-        physical_width: size.width as u32,
-        physical_height: size.height as u32,
-        scale_factor: window.scale_factor(),
-        font_definitions: FontDefinitions::default(),
-        style: Default::default(),
-    });
+    let mut state = State::new(&window);
+    state.set_pixels_per_point(window.scale_factor() as f32);
 
     // We use the egui_wgpu_backend crate as the render backend.
-    let mut egui_rpass = RenderPass::new(&device, surface_format, 1);
+    let mut egui_rpass = Renderer::new(&device, surface_format, None, 1);
 
     // Display the demo application that ships with egui.
     let mut demo_app = egui_demo_lib::DemoWindows::default();
 
-    let start_time = Instant::now();
+    let context = egui::Context::default();
+
+    let _start_time = Instant::now();
     event_loop.run(move |event, _, control_flow| {
         // Pass the winit events to the platform integration.
-        platform.handle_event(&event);
+        if let WindowEvent { event, .. } = &event {
+            let response = state.on_event(&context, event);
+            if response.repaint {
+                window.request_redraw();
+            }
+            if response.consumed {
+                return;
+            }
+        }
 
         match event {
             RedrawRequested(..) => {
-                platform.update_time(start_time.elapsed().as_secs_f64());
-
                 let output_frame = match surface.get_current_texture() {
                     Ok(frame) => frame,
                     Err(wgpu::SurfaceError::Outdated) => {
@@ -114,14 +126,17 @@ fn main() {
                     .create_view(&wgpu::TextureViewDescriptor::default());
 
                 // Begin to draw the UI frame.
-                platform.begin_frame();
+                let input = state.take_egui_input(&window);
+                context.begin_frame(input);
 
                 // Draw the demo application.
-                demo_app.ui(&platform.context());
+                demo_app.ui(&context);
 
                 // End the UI frame. We could now handle the output and draw the UI with the backend.
-                let full_output = platform.end_frame(Some(&window));
-                let paint_jobs = platform.context().tessellate(full_output.shapes);
+                let full_output = context.end_frame();
+                let paint_jobs = context.tessellate(full_output.shapes);
+
+                state.handle_platform_output(&window, &context, full_output.platform_output);
 
                 let mut encoder = device.create_command_encoder(&wgpu::CommandEncoderDescriptor {
                     label: Some("encoder"),
@@ -129,35 +144,46 @@ fn main() {
 
                 // Upload all resources for the GPU.
                 let screen_descriptor = ScreenDescriptor {
-                    physical_width: surface_config.width,
-                    physical_height: surface_config.height,
-                    scale_factor: window.scale_factor() as f32,
+                    size_in_pixels: [surface_config.width, surface_config.height],
+                    pixels_per_point: window.scale_factor() as f32,
                 };
                 let tdelta: egui::TexturesDelta = full_output.textures_delta;
-                egui_rpass
-                    .add_textures(&device, &queue, &tdelta)
-                    .expect("add texture ok");
-                egui_rpass.update_buffers(&device, &queue, &paint_jobs, &screen_descriptor);
+                for (tid, deltas) in tdelta.set {
+                    egui_rpass.update_texture(&device, &queue, tid, &deltas);
+                }
 
-                // Record all render passes.
-                egui_rpass
-                    .execute(
-                        &mut encoder,
-                        &output_view,
-                        &paint_jobs,
-                        &screen_descriptor,
-                        Some(wgpu::Color::BLACK),
-                    )
-                    .unwrap();
+                egui_rpass.update_buffers(
+                    &device,
+                    &queue,
+                    &mut encoder,
+                    &paint_jobs,
+                    &screen_descriptor,
+                );
+
+                let color_attach = wgpu::RenderPassColorAttachment {
+                    view: &output_view,
+                    resolve_target: None,
+                    ops: Default::default(),
+                };
+                let renderpass_descriptor = wgpu::RenderPassDescriptor {
+                    color_attachments: &[Some(color_attach)],
+                    ..Default::default()
+                };
+                let mut render_pass = encoder.begin_render_pass(&renderpass_descriptor);
+
+                egui_rpass.render(&mut render_pass, &paint_jobs, &screen_descriptor);
+
+                drop(render_pass);
+
                 // Submit the commands.
                 queue.submit(iter::once(encoder.finish()));
 
                 // Redraw egui
                 output_frame.present();
 
-                egui_rpass
-                    .remove_textures(tdelta)
-                    .expect("remove texture ok");
+                for tid in tdelta.free {
+                    egui_rpass.free_texture(&tid);
+                }
 
                 // Support reactive on windows only, but not on linux.
                 // if _output.needs_repaint {
